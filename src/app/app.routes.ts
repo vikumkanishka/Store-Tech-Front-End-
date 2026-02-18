532d3e8b6bf4117b6709dd255c024de895d81fe8
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-18 11:18:34 +0530

    feat: define application routes for dashboard and login components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './page/dashboard/dashboard';
+import { Login } from './page/login/login';
+import { Item } from './page/dashboard/item/item';
+import { Order } from './page/dashboard/order/order';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"dashboard",
+        component:Dashboard,
+        children:[
+            {
+                path:"customer",
+                component:Dashboard
+            },
+            {
+                path:"item",
+                component:Item
+            },
+            {
+                path:"order",
+                component:Order
+            }
+        ]
+    },
+    {
+        path:"login",
+        component:Login
+    }
+];
