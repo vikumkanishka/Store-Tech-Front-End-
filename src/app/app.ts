d43eefd0f9ba95fe9cc4c7fd7bcd26d1c579ad1d
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-18 10:31:44 +0530

    feat: implement OnInit lifecycle hook to initialize Flowbite in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite/lib/esm/components';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
   protected readonly title = signal('StoreTech');
 }
