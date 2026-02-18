be80405c3ca208be265ee86317403a0d09545078
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-18 11:00:43 +0530

    feat: add customer, dashboard, item, order, and login components with associated styles and tests

---
+++ src/app/page/dashboard/item/item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  imports: [],
+  templateUrl: './item.html',
+  styleUrl: './item.css',
+})
+export class Item {
+
+}
