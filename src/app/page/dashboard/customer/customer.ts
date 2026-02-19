b2b49fd81a10908a9c4a9b5eee4e37e2b0b24410
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-19 15:04:58 +0530

    feat: bind customer form fields to customerObj and integrate FormsModule for two-way data binding

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomerModel } from '../../../../model/types';
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-customer',
-  imports: [RouterOutlet, NgForOf],
+  imports: [RouterOutlet, NgForOf, FormsModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
@@ -29,5 +30,27 @@ export class Customer implements OnInit {
       this.customerList = response;
       this.cdr.detectChanges();
     });
+  }
+
+
+  customerObj : CustomerModel = {
+    id: '',
+    title: '',
+    name: '',
+    dob: {},
+    salary: 0,
+    address: '',
+    city: '',
+    province: '',
+    postalCode: ''
+  }
+
+  addCustomer():void{
+    console.log(this.customerObj);
+    
+    this.http.post("http://localhost:8080/customer/addCustomer",this.customerObj).subscribe(res=>{
+      console.log(res);
+      
+    })
   }
 }
