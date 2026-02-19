e73384006eab3d12fc80561bf76ed7d250db91a0
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-19 11:39:45 +0530

    feat: implement customer and item management pages with forms and data display

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-customer',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
-export class Customer {
+export class Customer implements OnInit {
 
+  customerList:any = null;
+
+  constructor(private http: HttpClient,private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this.http.get("http://localhost:8080/customer/getallcustomers").subscribe((response) => {
+      console.log(response);
+      this.customerList = response;
+      this.cdr.detectChanges();
+    });
+  }
 }
